54cb98d65ba917e6066d93cd1045d6b51893f018
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-29 15:43:13 +0900

    공공데이터포털 상권정보 주소와 지방행정인허가데이터 주소 비교 (경도위도때문에)

---
+++ coordinates.py
@@ -0,0 +1,38 @@
+import csv
+from multiprocessing import Process
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+
+localSameAddress = client['localSameAddress']
+
+
+def seoul_busan():
+    same_address = localSameAddress['same_address']
+    f = open('./prev_csv_data/상가업소정보_201912_01.csv', 'r', encoding='utf-8')
+    firstLine = f.readline()
+    rdr = csv.reader(f)
+
+    cnt = 0
+    current_count = 0
+    for line in rdr:
+        current_count += 1
+        print(current_count)
+        result = line[0].split("|")
+        if len(result) == 39:
+            rdnmAdr = result[31]
+            for local in same_address.find({"$or": [{"city": "seoul"}, {"city": "busan"}]}):
+                split1 = local['address'].split(',')[0]
+                split2 = split1.split("(")[0]
+                local_address = split2.strip()
+                # print(local_address, rdnmAdr)
+
+                if local_address == rdnmAdr:
+                    cnt += 1
+                    print(cnt, result, local)
+                    break
+
+
+
+
+seoul_busan()
