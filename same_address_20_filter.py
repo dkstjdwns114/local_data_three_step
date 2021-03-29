812f52f9b92358ab6206847f5968daed190efee3
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-29 15:43:57 +0900

    2020년 폐업한 가게가 2개 이상인 주소 필터링

---
+++ same_address_20_filter.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+localSameAddress = client['localSameAddress']
+
+
+def test_func():
+    data = localSameAddress['same_address_20']
+
+    total_cnt = data.count_documents({})
+
+    cnt = 0
+    for info in data.find({}).batch_size(1):
+        current_data = localSameAddress['same_address']
+        for is_overlap in current_data.find({"address": info['address'], "_id": {"$ne": info['_id']}}):
+            current_data.delete_one({"_id": is_overlap['_id']})
+        cnt += 1
+        print(cnt, "/", total_cnt, info)
+
+
+test_func()
