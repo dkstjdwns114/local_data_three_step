7f3317ead97cf69561d9143dc4c6a8288fa53685
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-01 19:29:35 +0900

    지난 1주일 데이터 DB에 저장

--- week_get_data.py
+++ week_get_data.py
@@ -2,6 +2,7 @@ from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 from pymongo import MongoClient
 import xmltodict, json
+from multiprocessing import Process
 
 client = MongoClient("localhost", 27017)
 
@@ -39,32 +40,99 @@ def close():
 
     jsonObj = json.loads(jsonString)
 
+    cnt = 0
     for item in jsonObj['rows']['row']:
         if item['dcbYmd'] == "20210331":
-            print(item)
-            print("번호", item['rowNum'])
-            print("개방자치단체코드", item['opnSfTeamCode'])
-            print("개방서비스ID", item['opnSvcId'])
-            print("데이터갱신일자", item['updateDt'])
-            print("개방서비스명", item['opnSvcNm'])
-            print("사업장명", item['bplcNm'])
-            print("도로명주소", item['rdnWhlAddr'])
-            print("인허가일자", item['apvPermYmd'])
-            print("인허가취소일자", item['apvCancelYmd'])
-            print("영업상태코드", item['trdStateGbn'])
-            print("영업상태명", item['trdStateNm'])
-            print("폐업일자", item['dcbYmd'])
-            print("업태구분명", item['uptaeNm'])
-            print("-------------------------")
+            cnt += 1
+            city_name = ""
+            category_name = ""
+            city_split = ""
+            if item['rdnWhlAddr']:
+                city_split = item['rdnWhlAddr'].split(' ')[0]
+                address = item['rdnWhlAddr']
+            else:
+                city_split = item['siteWhlAddr'].split(' ')[0]
+                address = item['siteWhlAddr']
+
+            if city_split == "부산광역시":
+                city_name = "busan"
+            elif city_split == "충청북도":
+                city_name = "chungbuk"
+            elif city_split == "충청남도":
+                city_name = "chungnam"
+            elif city_split == "대구광역시":
+                city_name = "daegu"
+            elif city_split == "대전광역시":
+                city_name = "daejeon"
+            elif city_split == "강원도":
+                city_name = "gangwon"
+            elif city_split == "광주광역시":
+                city_name = "gwangju"
+            elif city_split == "경기도":
+                city_name = "gyeonggi"
+            elif city_split == "경상북도":
+                city_name = "gyeongbuk"
+            elif city_split == "경상남도":
+                city_name = "gyeongnam"
+            elif city_split == "인천광역시":
+                city_name = "incheon"
+            elif city_split == "제주특별자치도":
+                city_name = "jeju"
+            elif city_split == "전라북도":
+                city_name = "jeonbuk"
+            elif city_split == "전라남도":
+                city_name = "jeonnam"
+            elif city_split == "세종특별자치시":
+                city_name = "sejong"
+            elif city_split == "서울특별시":
+                city_name = "seoul"
+            elif city_split == "울산광역시":
+                city_name = "ulsan"
+
+            open_service_id_split = item['opnSvcId'][0:2]
+
+            if open_service_id_split == "01":
+                category_name = "health"
+            elif open_service_id_split == "02":
+                category_name = "animal"
+            elif open_service_id_split == "03" or open_service_id_split == "04":
+                category_name = "culture"
+            elif open_service_id_split == "05" or open_service_id_split == "06" or open_service_id_split == "08" or open_service_id_split == "10":
+                category_name = "life"
+            elif open_service_id_split == "07":
+                category_name = "food"
+            elif open_service_id_split == "09":
+                category_name = "environment"
+            elif open_service_id_split == "11":
+                category_name = "other"
+
+            info = {
+                "data": "close",
+                "update_date": item['updateDt'],
+                "authorization_date": item['apvPermYmd'],
+                "closed_date": item['dcbYmd'],
+                "store_name": item['bplcNm'],
+                "address": address,
+                "state_code": item['trdStateGbn'],
+                "state": item['trdStateNm'],
+                "open_service_id": item['opnSvcId'],
+                "open_service": item['opnSvcNm'],
+                "detailed_classification": item['uptaeNm'],
+                "city_name": city_name,
+                "category_name": category_name,
+            }
+            print(cnt, "close", info)
+            local_real_time_close = localWeekData['local_real_time_close']
+            local_real_time_close_id = local_real_time_close.insert_one(info).inserted_id
 
 
 def today_open():
     queryParams = '?' + urlencode({
         quote_plus("authKey"): "uZ98I03VR/zZ3zAvdNIVEl8EWJy6llSwLtDAMDlOYkE=",
 
-        quote_plus("bgnYmd"): "20210330",
+        quote_plus("bgnYmd"): "20210331",
 
-        quote_plus("endYmd"): "20210330",
+        quote_plus("endYmd"): "20210331",
 
         quote_plus("state"): "01",
 
@@ -85,23 +153,101 @@ def today_open():
 
     jsonObj = json.loads(jsonString)
 
+    cnt = 0
     for item in jsonObj['rows']['row']:
-        print(item)
-        print("번호", item['rowNum'])
-        print("개방자치단체코드", item['opnSfTeamCode'])
-        print("개방서비스ID", item['opnSvcId'])
-        print("데이터갱신일자", item['updateDt'])
-        print("개방서비스명", item['opnSvcNm'])
-        print("사업장명", item['bplcNm'])
-        print("도로명주소", item['rdnWhlAddr'])
-        print("인허가일자", item['apvPermYmd'])
-        print("인허가취소일자", item['apvCancelYmd'])
-        print("영업상태코드", item['trdStateGbn'])
-        print("영업상태명", item['trdStateNm'])
-        print("폐업일자", item['dcbYmd'])
-        print("업태구분명", item['uptaeNm'])
-        print("-------------------------")
+        if item['apvPermYmd'] == "20210331":
+            cnt += 1
+            city_name = ""
+            category_name = ""
+            city_split = ""
+            if item['rdnWhlAddr']:
+                city_split = item['rdnWhlAddr'].split(' ')[0]
+                address = item['rdnWhlAddr']
+            else:
+                city_split = item['siteWhlAddr'].split(' ')[0]
+                address = item['siteWhlAddr']
+
+            if city_split == "부산광역시":
+                city_name = "busan"
+            elif city_split == "충청북도":
+                city_name = "chungbuk"
+            elif city_split == "충청남도":
+                city_name = "chungnam"
+            elif city_split == "대구광역시":
+                city_name = "daegu"
+            elif city_split == "대전광역시":
+                city_name = "daejeon"
+            elif city_split == "강원도":
+                city_name = "gangwon"
+            elif city_split == "광주광역시":
+                city_name = "gwangju"
+            elif city_split == "경기도":
+                city_name = "gyeonggi"
+            elif city_split == "경상북도":
+                city_name = "gyeongbuk"
+            elif city_split == "경상남도":
+                city_name = "gyeongnam"
+            elif city_split == "인천광역시":
+                city_name = "incheon"
+            elif city_split == "제주특별자치도":
+                city_name = "jeju"
+            elif city_split == "전라북도":
+                city_name = "jeonbuk"
+            elif city_split == "전라남도":
+                city_name = "jeonnam"
+            elif city_split == "세종특별자치시":
+                city_name = "sejong"
+            elif city_split == "서울특별시":
+                city_name = "seoul"
+            elif city_split == "울산광역시":
+                city_name = "ulsan"
+
+            open_service_id_split = item['opnSvcId'][0:2]
+
+            if open_service_id_split == "01":
+                category_name = "health"
+            elif open_service_id_split == "02":
+                category_name = "animal"
+            elif open_service_id_split == "03" or open_service_id_split == "04":
+                category_name = "culture"
+            elif open_service_id_split == "05" or open_service_id_split == "06" or open_service_id_split == "08" or open_service_id_split == "10":
+                category_name = "life"
+            elif open_service_id_split == "07":
+                category_name = "food"
+            elif open_service_id_split == "09":
+                category_name = "environment"
+            elif open_service_id_split == "11":
+                category_name = "other"
+
+            info = {
+                "data": "open",
+                "update_date": item['updateDt'],
+                "authorization_date": item['apvPermYmd'],
+                "closed_date": item['dcbYmd'],
+                "store_name": item['bplcNm'],
+                "address": item['rdnWhlAddr'],
+                "state_code": item['trdStateGbn'],
+                "state": item['trdStateNm'],
+                "open_service_id": item['opnSvcId'],
+                "open_service": item['opnSvcNm'],
+                "detailed_classification": item['uptaeNm'],
+                "city_name": city_name,
+                "category_name": category_name,
+            }
+
+            print(cnt, "open", info)
+
+            local_real_time_open = localWeekData['local_real_time_open']
+            local_real_time_open_id = local_real_time_open.insert_one(info).inserted_id
 
 
-close()
-# today_open()
+if __name__ == "__main__":
+    p1 = Process(target=close)
+    p2 = Process(target=today_open)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
